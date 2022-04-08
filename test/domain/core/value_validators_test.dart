9ee39c688dad52c9de72ae6345566b2d74659ba6
Gabriel Carneiro <gabcarneiro@outlook.com>
2022-04-08 00:44:04 -0300

    test: adds tests for validateMaxStringLength

--- test/domain/core/value_validators_test.dart
+++ test/domain/core/value_validators_test.dart
@@ -1,7 +1,45 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:notes_firebase_ddd_course/domain/core/failures.dart';
+import 'package:notes_firebase_ddd_course/domain/core/value_validators.dart';
 
 void main() {
-  testWidgets('value validators ...', (tester) async {
-    // TODO: Implement test
+  group('validateMaxStringLength', () {
+    test(
+      'should return right with string input '
+      'when input string lenght is lesser than maxLenght',
+      () {
+        final result = validateMaxStringLength('string', 7);
+
+        expect(result, equals(const Right('string')));
+      },
+    );
+
+    test(
+      'should return right with string input '
+      'when input string lenght is equals to maxLenght',
+      () {
+        final result = validateMaxStringLength('string', 6);
+
+        expect(result, equals(const Right('string')));
+      },
+    );
+
+    test(
+      'should return left with ExceedingLength '
+      'when input string lenght is greater to maxLenght',
+      () {
+        final result = validateMaxStringLength('string', 5);
+
+        expect(
+          result,
+          equals(
+            const Left(
+              ValueFailure.exceedingLength(failedValue: 'string', max: 5),
+            ),
+          ),
+        );
+      },
+    );
   });
 }
